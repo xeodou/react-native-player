4a481f82d28a73b0a03600e8d1bafe676eeabe59
xeodou <xeodou@gmail.com>
2015-10-27 18:28:26 +0800

    Change name.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+* react-native-audio - index.android.js
+* Copyright(c) 2015 xeodou <xeodou@gmail.com>
+* MIT Licensed
+*/
+
+var { NativeModules } = require('react-native');
+module.exports = NativeModules.ReactAudio;
