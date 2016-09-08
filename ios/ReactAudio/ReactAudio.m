7644cae0de8c94350a89061228e13682b00dc6df
good2hear <umhuh86@gmail.com>
2016-09-08 11:19:08 +0900

    - apply 'seekTo' and 'stop' - fixing 'getDuration' and 'currentTime'

--- ios/ReactAudio/ReactAudio.m
+++ ios/ReactAudio/ReactAudio.m
@@ -17,6 +17,7 @@
   NSTimer *timer;
   CMTime duration;
   CMTime currentTime;
+
 }
 
 @end
@@ -33,11 +34,17 @@ RCT_EXPORT_METHOD(prepare:(NSString *)url:(BOOL) bAutoPlay) {
   
   if(!([url length]>0)) return;
   
+  [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+  [[AVAudioSession sharedInstance] setActive: YES error: nil];
+  [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+    
+  [self.bridge.eventDispatcher sendDeviceEventWithName: @"onPlayerStateChanged"
+                                                  body: @{@"playbackState": @4 }];
+
   NSURL *soundUrl = [[NSURL alloc] initWithString:url];
   self.playerItem = [AVPlayerItem playerItemWithURL:soundUrl];
   self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
 
-  
   [[NSNotificationCenter defaultCenter]
   	addObserver:self selector:@selector(playFinished:) name:AVPlayerItemDidPlayToEndTimeNotification object:self.playerItem];
 
@@ -60,12 +67,14 @@ RCT_EXPORT_METHOD(prepare:(NSString *)url:(BOOL) bAutoPlay) {
 RCT_EXPORT_METHOD(stop) {
 
   [self.player pause];
-  self.player.rate = 0;
-  timer.invalidate;
+  CMTime newTime = CMTimeMakeWithSeconds(0, 1);
+  [self.player seekToTime:newTime];
+
 }
 
 RCT_EXPORT_METHOD(seekTo:(int) nSecond) {
-    [self.player seekToTime: CMTimeMakeWithSeconds(nSecond, 1)];
+  CMTime newTime = CMTimeMakeWithSeconds(nSecond, 1);
+  [self.player seekToTime:newTime];
 }
 
 RCT_EXPORT_METHOD(getDuration:(RCTResponseSenderBlock)callback){
@@ -74,7 +83,8 @@ RCT_EXPORT_METHOD(getDuration:(RCTResponseSenderBlock)callback){
   }
   
   float duration = CMTimeGetSeconds(self.playerItem.duration);
-  callback(@[[[NSNumber alloc] initWithFloat:duration]]);
+  float durationInMilliSeconds = duration * 1000;
+  callback(@[[[NSNumber alloc] initWithFloat:durationInMilliSeconds]]);
 }
 
 
@@ -88,19 +98,28 @@ RCT_EXPORT_METHOD(play) {
   });
 }
 
--(void) startSending: (NSTimer *)timer {
+RCT_EXPORT_METHOD(resume) {
   
-  duration = self.player.currentItem.duration;
-  currentTime = self.player.currentItem.currentTime;
+  [self.player play];
   
-  [self.bridge.eventDispatcher sendDeviceEventWithName: @"playListener" body: @{@"currentTime": @(CMTimeGetSeconds(currentTime)) }];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(startSending:) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:timer forMode:NSRunLoopCommonModes];
+  });
 }
 
+-(void) startSending: (NSTimer *)timer {
+
+  currentTime = self.player.currentItem.currentTime;  
+  [self.bridge.eventDispatcher sendDeviceEventWithName: @"onUpdatePosition" 
+            body: @{@"currentPosition": @(CMTimeGetSeconds(currentTime)*1000) }];
+}
 
 RCT_EXPORT_METHOD(pause) {
   
   [self.player pause];
-  timer.invalidate;
+  [timer invalidate];
+  timer = nil;
   
 }
 
